cfe3ffd4cbed02e1e6177679ebb6babbc420a990
Joseph Magen <joseph@isratrade.co.il>
2013-11-03 19:29:30 +0000

    app controller add login

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_admin_user
+
+  def set_admin_user
+    User.current               = User.admin
+    session[:user]             = User.current.id
+  end
+
+  def current_user
+  	User.current
+  end
+
+
 end
