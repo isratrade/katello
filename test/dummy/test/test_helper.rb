61c9b5c63edb791ec41f2bd701b5f55b84ab5713
Joseph Magen <joseph@isratrade.co.il>
2013-11-03 20:44:40 +0000

    test helper dummy

--- test/dummy/test/test_helper.rb
+++ test/dummy/test/test_helper.rb
@@ -3,14 +3,7 @@ require 'spork'
 # $LOAD_PATH required for testdrb party of spork-minitest
 $LOAD_PATH << "test"
 
-unless RUBY_VERSION =~ /^1\.8/
-  require 'simplecov'
-  SimpleCov.start 'rails' do
-    add_group 'API', 'app/controllers/api'
-  end
-end
-
-Spork.prefork do
+#Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
@@ -118,75 +111,13 @@ Spork.prefork do
     end
   end
 
-  # Transactional fixtures do not work with Selenium tests, because Capybara
-  # uses a separate server thread, which the transactions would be hidden
-  # from. We hence use DatabaseCleaner to truncate our test database.
-  DatabaseCleaner.strategy = :truncation
-
-  class ActionDispatch::IntegrationTest
-    # Make the Capybara DSL available in all integration tests
-    include Capybara::DSL
-
-    # Stop ActiveRecord from wrapping tests in transactions
-    self.use_transactional_fixtures = false
-
-    def assert_index_page(index_path,title_text,new_link_text = nil,has_search = true,has_pagination = true)
-      visit index_path
-      assert page.has_selector?('h1', :text => title_text), "#{title_text} was expected in the <h1> tag, but was not found"
-      (assert find_link(new_link_text).visible?, "#{new_link_text} is not visible") if new_link_text
-      (assert find_button('Search').visible?, "Search button is not visible") if has_search
-      (assert has_content?("Displaying"), "Pagination 'Display ...' does not appear") if has_pagination
-    end
-
-    def assert_new_button(index_path,new_link_text,new_path)
-      visit index_path
-      click_link new_link_text
-      assert_equal new_path, current_path, "new path #{new_path} was expected but it was #{current_path}"
-    end
-
-    def assert_submit_button(redirect_path,button_text = "Submit")
-      click_button button_text
-      assert_equal redirect_path, current_path, "redirect path #{redirect_path} was expected but it was #{current_path}"
-    end
-
-    def assert_delete_row(index_path, link_text, delete_text = "Delete", dropdown = false)
-      visit index_path
-      within(:xpath, "//tr[contains(.,'#{link_text}')]") do
-        find("i.caret").click if dropdown
-        click_link(delete_text)
-      end
-      popup = page.driver.browser.switch_to.alert
-      popup.accept
-      assert page.has_no_link?(link_text), "link '#{link_text}' was expected NOT to be on the page, but it was found."
-      assert page.has_content?('Successfully destroyed'), "flash message 'Successfully destroyed' was expected but it was not found on the page"
-    end
-
-    def assert_cannot_delete_row(index_path, link_text, delete_text = "Delete", dropdown = false, flash_message = true)
-      visit index_path
-      within(:xpath, "//tr[contains(.,'#{link_text}')]") do
-        find("i.caret").click if dropdown
-        click_link(delete_text)
-      end
-      popup = page.driver.browser.switch_to.alert
-      popup.accept
-      assert page.has_link?(link_text), "link '#{link_text}' was expected but it was not found on the page."
-      assert page.has_content?("is used by"), "flash message 'is used by' was expected but it was not found on the page."
-    end
-
-    def fix_mismatches
-      Location.all_import_missing_ids
-      Organization.all_import_missing_ids
-    end
-
-  end
-
   class ActionView::TestCase
     helper Rails.application.routes.url_helpers
   end
 
-end
+#end
 
-Spork.each_run do
+#Spork.each_run do
   # This code will be run each time you run your specs.
   class ActionController::TestCase
     setup :setup_set_script_name, :set_api_user, :reset_setting_cache
@@ -205,25 +136,4 @@ Spork.each_run do
     end
   end
 
-  class ActionDispatch::IntegrationTest
-
-    setup :login_admin
-
-    teardown do
-      DatabaseCleaner.clean       # Truncate the database
-      Capybara.reset_sessions!    # Forget the (simulated) browser state
-      Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
-    end
-
-    private
-
-    def login_admin
-      visit "/"
-      fill_in "login_login", :with => "admin"
-      fill_in "login_password", :with => "secret"
-      click_button "Login"
-    end
-
-  end
-
-end
+#end
