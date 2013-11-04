b60b89b3b2922aa4eb1d3bb381d37be9bd728b0b
Joseph Magen <joseph@isratrade.co.il>
2013-11-04 12:48:56 +0000

    Engine: isolate_namespace Katello

--- lib/katello/engine.rb
+++ lib/katello/engine.rb
@@ -1,6 +1,7 @@
 module Katello
 
   class Engine < ::Rails::Engine
+    isolate_namespace Katello
     engine_name 'katello'
 
     initializer "katello.simple_navigation" do |app|
@@ -37,8 +38,18 @@ module Katello
     end
 
     config.to_prepare do
+      FastGettext.add_text_domain('katello', {
+        :path => File.expand_path("../../../locale", __FILE__),
+        :type => :po,
+        :ignore_fuzzy => true,
+        :report_warning => false
+        })
+      FastGettext.default_text_domain = 'katello'
+
       # Model extensions
       ::User.send :include, Katello::Concerns::UserExtensions
+
+
     end
 
     rake_tasks do
@@ -49,18 +60,6 @@ module Katello
       load "#{Katello::Engine.root}/lib/katello/tasks/jenkins.rake"
     end
 
-  end
-
-  def table_name_prefix
-    'katello_'
-  end
-
-  def use_relative_model_naming
-    true
-  end
-
-  def self.table_name_prefix
-    'katello_'
   end
 
 end
